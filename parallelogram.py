f3c1dc6d0d92ee67543b60ff51875eea861b3a7b
hihumi <yui.maa5800@gmail.com>
2017-01-26 16:37:47 +0900

    add file

---
+++ parallelogram.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+"""turtleで平行四辺形を描画
+"""
+
+
+from turtle import *
+
+
+def parallelogram():
+    """turtleで平行四辺形を描画する関数
+    """
+
+    for i in range(2):
+        print('#', i)
+        forward(200)
+        left(60)
+        if i % 1 == 0:
+            print('##', i)
+            forward(100)
+            left(120)
+
+    input_data = input('終了: enterキー等: ')
+
+if __name__ == '__main__':
+    parallelogram()
